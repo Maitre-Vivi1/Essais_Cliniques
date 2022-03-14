76cbdfb36aceeb12707d9aba1a43fba02fa16f44
vivi1 <viviendupont21@gmail.com>
2022-03-14 15:48:43 +0100

    Graph du rapport

--- Double_critère.R
+++ Double_critère.R
@@ -60,25 +60,29 @@ calcul_go_nogo_indeter <- function(n, VE_seuil1, VE_seuil2, proba1, proba2, VE_t
   return(c(go, nogo, indeter))
 }
 
-
-test <- as.data.frame(t(sapply(seq(0.501,0.9,0.001), calcul_go_nogo_indeter, n = 70, VE_seuil1 = 0.3, VE_seuil2 = 0.6, proba1 = 0.99, proba2 = 0.7, nb_replic = 1000)))
-names(test) <- c("go", "nogo", "indeter")
-
 sequ <- seq(0.501,0.9,0.001)
 
-plot(x = sequ, y = test$go, "l", ylab = "Probability", xlab = "Vraie VE", main = " n(vaccin) = n(placebo) = 35")
-lines(x = sequ, y = test$nogo, "l" , lty=2, lwd=1)
-lines(x = sequ, y = test$indeter, "l", lty=2, lwd=3)
-abline(v=0.7, col = "red")
-abline(h = 0.95, col = "blue")
+set.seed(123)
 
-sequ[which(test$go > 0.95)][1]
+test <- as.data.frame(t(sapply(seq(0.501,0.9,0.001), calcul_go_nogo_indeter, n = 39, VE_seuil1 = 0.5, VE_seuil2 = 0.8, proba1 = 0.99, proba2 = 0.2, nb_replic = 1000)))
+test <- cbind(test, sequ)
+names(test) <- c("go", "nogo", "indeter", "VE")
 
-test$go[which(sequ == 0.7)]
+
 
 test$go <- ksmooth(sequ, test$go, kernel = "normal", bandwidth = 0.01)$y
 test$nogo <- ksmooth(sequ, test$nogo, kernel = "normal", bandwidth = 0.01)$y
 test$indeter <- ksmooth(sequ, test$indeter, kernel = "normal", bandwidth = 0.01)$y
+
+test$go[which(test$VE == 0.9)]
+
+sequ[which(test$go > 0.8)][1]
+
+plot(x = sequ, y = test$go, "l", ylab = "Probability", xlab = "Vraie VE", main = " n(vaccin) = n(placebo) = 35")
+lines(x = sequ, y = test$nogo, "l" , lty=2, lwd=1)
+lines(x = sequ, y = test$indeter, "l", lty=2, lwd=3)
+abline(v=0.8, col = "red")
+abline(h = 0.95, col = "blue")
 
 library(ggplot2)
 
@@ -86,10 +90,13 @@ ggplot(data = test, aes(x = sequ)) +
   geom_line(aes(y = go, col = "go"), size = 1.25) +
   geom_line(aes(y = nogo, col = "no go"), size = 1.25) +
   geom_line(aes(y = indeter, col = "cannot decide"), size = 1.25) +
-  geom_hline(yintercept = test$go[which(sequ == 0.7)], col = "black") +
-  geom_vline(xintercept = 0.7, col = "black") +
+  geom_hline(yintercept = test$go[which(sequ == 0.8)], col = "black") +
+  geom_vline(xintercept = 0.8, col = "black") +
   scale_color_manual(name = "Decision", values = c("go" = "darkblue", "no go" = "red", "cannot decide" = "brown")) +
   xlab("VE") +
-  ylab("probability")
+  ylab("probability") +
+  scale_y_continuous(breaks = c(0.00,0.25,0.50,0.75,0.95,1.00)) +
+  ggtitle("n(vaccin) = n(placebo) = 37")
 
 35/0.017/0.8*2
+
